b4b7826cc7d27c1bab40239aaa2038845dd7a515
2044smile <2044smile@naver.com>
2019-10-08 15:20:21 +0900

    배포 전 마지막 Save, 주석추가 코드정리

--- rank_crawling.py
+++ rank_crawling.py
@@ -1,9 +1,5 @@
 import time, os, requests
-from datetime import datetime
 from bs4 import BeautifulSoup
-from termcolor import *
-import colorama
-colorama.init() # color
 # Python이 실행될 때 DJANGO_SETTINGS_MODULE이라는 환경 변수에 현재 프로젝트의 settings.py파일 경로를 등록합니다
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Trend.settings")
 
@@ -27,12 +23,8 @@ def naver_trend():
         rank_title = soup.select( # 네이버 인기검색어의 이름 ex) 조국 . . . 1~20등 까지의 모든 title
             '#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul > li > a > span.ah_k'
         )
-        # rank_num = soup.select( # 네이버 인기검색어의 순위 ex) 1  . . . 1~20까지의 모든 number
-        #     '#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul > li > a > span.ah_r'
-        # )
         for i in range(20): # 20위 까지 받기 위해서
             rank_data[i] = rank_title[i].text # rank_num[i].text.zfill(2):
-            # cprint(str(rank_num[i].text.zfill(2)) + ' ' + rank_title[i].text, 'yellow') # ex) 01 조국 노란색으로 출력
 
         link = {}
         ranklink = {}
@@ -43,13 +35,12 @@ def naver_trend():
             link_text = a['href']  # 1~40 까지의 값이 들어간다 1~20까지는 #의 값이 21~40까지는 실제 link
             link[x] = link_text
 
-        for y in range(21, 41):
+        for y in range(21, 41): # 1~20 까지는 # 21~40까지 실제 데이터가 들어있다.
             ranklink[v] = link[y]
             v += 1
 
         NaverRank.objects.all().delete()
         rank_data_dict = rank_data  # naver_trend 크롤링 돌아가는 함수 부분
-        # for n, t in rank_data_dict.items():
         for n, t in rank_data_dict.items():
             NaverRank(rank_num=n+1, rank_title=t, url=ranklink[n]).save()
         return print('Naver 업로드 완료')
@@ -67,9 +58,6 @@ def daum_trend():
         rank_title = soup.select(  # 네이버 인기검색어의 이름 ex) 조국 . . . 1~20등 까지의 모든 title
             '#mArticle > div.cmain_tmp > div.section_media > div.hot_issue.issue_mini > div.hotissue_mini > ol > li > div > div > span.txt_issue'
         )
-        # rank_num = soup.select(  # 네이버 인기검색어의 순위 ex) 1  . . . 1~20까지의 모든 number
-        #     '#mArticle > div.cmain_tmp > div.section_media > div.hot_issue.issue_mini > div.hotissue_mini > ol > li > div > div > span.num_pctop > span'
-        # )
         for i in range(10):  # 20위 까지 받기 위해서
             rank_data[i] = rank_title[i].text
 
@@ -83,7 +71,7 @@ def daum_trend():
             link_text = a['href']
             link[x] = link_text
 
-        for y in range(1, 20, 2):
+        for y in range(1, 20, 2): # 1 3 5 7 9 11 13 15 17 19
             ranklink[v] = link[y]
             v += 1
 
@@ -109,11 +97,12 @@ def google_trend():
         v = 0
         link = {}
         ranklink = {}
-        for linkElement in soup.findAll('ht:news_item_url'):
+        for linkElement in soup.findAll('ht:news_item_url'): # findAll xml 의 형식을 크롤링할 때 사용
             x += 1
             link[x] = linkElement.text
 
-        for y in range(1, 22, 2):
+        for y in range(1, 20, 2): # 1 3 5 7 9 11 13 15 17 19
+            # ex) 조국이라는 item에 2개의 링크가 존재하므로 2씩 증가시키면서 그 다음의 첫 번째 링크가 나오게 끔
             ranklink[v] = link[y]
             v += 1
         GoogleRank.objects.all().delete()
